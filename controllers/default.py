885c7167aa4f552648ec9c5f66e77ab81026f0fa
iMrJulian <36264252+iMrJulian@users.noreply.github.com>
2020-03-26 12:25:12 -0500

    Add files via upload

---
+++ controllers/default.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------------
+# This is a sample controller
+# this file is released under public domain and you can use without limitations
+# -------------------------------------------------------------------------
+
+# ---- example index page ----
+def index():
+    return dict(message=T('Welcome to web2py!'))
+
+def saltarin():
+    return dict(S = "")
+
+def paloma():
+    return dict()
+
+def buscaflores():
+    return dict()
+
+def cazacalaveras():
+    return dict()
+
+def m2048():
+    return dict()
+
+def covid():
+    return dict()
+
+def sugerencias():
+    return dict()
+
+def reviews():
+    return dict()
+
